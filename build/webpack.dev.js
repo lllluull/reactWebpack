bad656017023e611805b3527fc6cedb7b85b8a9a
hupengpeng <hupp@xiaoma.wang>
2019-07-02 18:11:59 +0800

    dev-server

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,12 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports  = {
     mode: 'development',      // 模式，表示dev环境
     entry: './src/index.js',  // 入口文件
-    module: {},               // 让 webpack 能够去处理那些非 JavaScript 文件
-    plugins: [],              // 插件
     output: {
         filename: 'bundle.js',  // 打包后文件名称
         path: path.resolve(__dirname, '../dist') // 打包后文件夹存放路径
-    }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '../dist'),
+        port: 8080,
+        hot: true,
+        open: true
+    },
+    module: {
+        rules: [{
+            test: /\.(js|mjs|jsx)$/,
+            exclude: /node_modules/, // 排除node_modules中的代码
+            use: [{
+                loader: 'babel-loader'
+            }],
+        }]
+    },             // 让 webpack 能够去处理那些非 JavaScript 文件
+    plugins: [
+        new HtmlWebpackPlugin({   // 向dist文件中自动添加模版html
+            template: 'src/index.html',
+        }),
+        new CleanWebpackPlugin(), // 打包后先清除dist文件，先于HtmlWebpackPlugin运行
+    ]              // 插件
+    
 }
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+
+const App = () => 
+    <div>你好啊</div>
+
+export default App
