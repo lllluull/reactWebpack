c55fe61864343cc94d7abdb0572074d69c0564da
hupengpeng <hupp@xiaoma.wang>
2019-07-03 15:03:17 +0800

    第二次更新

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -19,6 +19,8 @@ const DevConfig  = {
         hot: true,
         open: true,
         http2: true,
+        inline: true, // 启用内联模式，一段处理实时重载的脚本被插入到bundle中，并且构建消息会出现在浏览器控制台
+        clientLogLevel: 'none', // 阻止打印那种搞乱七八糟的控制台信息
         overlay: {
             warnings: true,
             errors: true
@@ -45,8 +47,28 @@ const DevConfig  = {
     },             // 让 webpack 能够去处理那些非 JavaScript 文件
     plugins: [
         new webpack.NamedModulesPlugin(),  //用于启动HMR时可以显示模块的相对路径
-        new webpack.HotModuleReplacementPlugin() // 开启模块热更新，热加载和模块热更新不同，热加载是整个页面刷新
-    ]         
+        new webpack.HotModuleReplacementPlugin(), // 开启模块热更新，热加载和模块热更新不同，热加载是整个页面刷新
+        // 简化控制台输出
+        new FriendlyErrorsWebpackPlugin({
+            compilationSuccessInfo: {
+            messages: [`You application is running here http://localhost:${devServer.port}`],
+            notes: [
+                'Some additionnal notes to be displayed unpon successful compilation',
+            ],
+            },
+    
+            // 是否应在每次编译之间清除控制台
+            clearConsole: true,
+            onErrors: function(severity, errors) {
+            // You can listen to errors transformed and prioritized by the plugin
+            // severity can be 'error' or 'warning'
+            },
+    
+            // 添加格式化程序和转换器（见下文）
+            additionalFormatters: [],
+            additionalTransformers: [],
+        }),
+        ]         
     
     
 }
